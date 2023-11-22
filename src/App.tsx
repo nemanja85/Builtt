05b396ed0b14f6376c77a3ae5d501d0af8130951
nemanja85 <mitic.nemanja@hotmail.com>
2023-11-22 15:28:45 +0100

    feat: Setup projects

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="w-full text-center">
+        <h1 className="text-lg font-bold">Hello Bootstrap</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
