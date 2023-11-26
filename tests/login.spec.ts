2d991d58108f5477f359d537edd51b9143044cf9
nemanja85 <mitic.nemanja@hotmail.com>
2023-11-26 23:08:20 +0100

    feat: Finish tests

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -8,3 +8,9 @@ test('Validation should show popup when fields are empty', async ({ page }) => {
   expect(await page.getByText('Polje email je obavezno.').isVisible()).toBeTruthy();
   expect(await page.getByText('Polje lozinka je obavezno.').isVisible()).toBeTruthy();
 });
+
+test('Unauthorized user access the shop page and get the message to login first', async ({ page }) => {
+  await page.goto('http://localhost:5173/shop');
+  await page.goto('http://localhost:5173/auth/login?message=Molimo%20vas%20da%20se%20ulogujete');
+  await expect(await page.getByText('Molimo vas da se ulogujete').isVisible()).toBeTruthy();
+});
