e930af8f980c3f41b77278179be0edbcf0c65f54
nemanja85 <mitic.nemanja@hotmail.com>
2023-11-26 23:25:16 +0100

    feat: Added test after successfully login redirect to Shop page

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -14,3 +14,14 @@ test('Unauthorized user access the shop page and get the message to login first'
   await page.goto('http://localhost:5173/auth/login?message=Molimo%20vas%20da%20se%20ulogujete');
   await expect(await page.getByText('Molimo vas da se ulogujete').isVisible()).toBeTruthy();
 });
+
+test('After successfully login redirect to Shop page', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  await page.goto('http://localhost:5173/auth/login');
+  await page.getByLabel('Email adresa').click();
+  await page.getByLabel('Email adresa').fill('admin@admin.com');
+  await page.getByLabel('Lozinka').click();
+  await page.getByLabel('Lozinka').fill('admin12345');
+  await page.getByRole('button', { name: 'Prijavi se na nalog' }).click();
+  await expect(page).toHaveURL(/shop/);
+});
